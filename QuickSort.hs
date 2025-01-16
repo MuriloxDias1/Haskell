3e5bc81b3e349463af34520fbb7f5af185d4bdd2
MuriloxDias1 <murilo.dias@discente.uenp.edu.br>
2025-01-16 00:19:07 -0300

    QuickSort.hs

--- QuickSort.hs
+++ QuickSort.hs
@@ -1,11 +1,12 @@
+-- Função QuickSort em Haskell
 quickSort :: (Ord a) => [a] -> [a]
-quickSort [] = [] 
+quickSort [] = []  -- Caso base: lista vazia já está ordenada
 quickSort (x:xs) = quickSort lesser ++ [x] ++ quickSort greater
   where
-    lesser = filter (< x) xs  
-    greater = filter (>= x) xs  
+    lesser = filter (< x) xs  -- Elementos menores que o pivô
+    greater = filter (>= x) xs  -- Elementos maiores ou iguais ao pivô
 
-
+-- Função principal para testar a implementação
 main :: IO ()
 main = do
     let lista = [5, 3, 8, 6, 2, 7, 4, 1]
